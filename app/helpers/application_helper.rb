422912168a62547fc8411b576264fa9973d14930
slcwestdevcamp <slcwest@devcamp.com>
2017-08-01 00:00:01 +0000

    Fixed bug with login helper method by setting defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
   
   def copyright_generator
-    AbdiViewTool::Renderer.copyright 'Luqman Abdi |', 'All rights reserved'
+    AbdiViewTool::Renderer.copyright '| Luqman Abdi |', 'All rights reserved'
   end
 end
