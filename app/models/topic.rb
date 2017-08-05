5d0569a655cc1d60ad7003e037adf9c907459d09
slcwestdevcamp <slcwest@devcamp.com>
2017-08-05 04:10:08 +0000

    Implemented icons in breadcrums

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,12 +2,8 @@ class Topic < ApplicationRecord
   validates_presence_of :title
 
   has_many :blogs
-  
+
   def self.with_blogs
     includes(:blogs).where.not(blogs: { id: nil })
   end
 end
-blog_filled_array = []
-topics = Topic.all
-topics.each do |topic|
-  blog_filled_array << topic if topic.blogs.count > 0
